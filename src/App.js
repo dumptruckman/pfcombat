89f82d0208765ba0819ffd095657aea516d1e157
Jeremy Wood <farachan@gmail.com>
2017-05-19 01:48:13 -0400

    Implemented a basic top level app state.

--- src/App.js
+++ src/App.js
@@ -6,15 +6,27 @@ import PartyEditor from "./components/PartyEditor";
 import EnemyEditor from "./components/EnemyEditor";
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            party: [new CombatantModel("Johnny"), new CombatantModel("Linus")],
+            enemies: [new CombatantModel("Orc 1"), new CombatantModel("Orc 2"), new CombatantModel("Orc Chieftan")],
+            combatants: []
+        }
+    }
+
+    componentWillMount() {
+        this.setState({combatants: this.state.party.concat(this.state.enemies)});
+    }
+
     render() {
-        let party = [new CombatantModel("Johnny"), new CombatantModel("Linus")];
-        let enemies = [new CombatantModel("Orc 1"), new CombatantModel("Orc 2"), new CombatantModel("Orc Chieftan")];
-        let combatants = party.concat(enemies);
+
         return (
             <div id="combat-tab" className="tab">
-                <InitiativeTracker combatants={combatants} />
-                <PartyEditor party={party} />
-                <EnemyEditor enemies={enemies} />
+                <InitiativeTracker combatants={this.state.combatants} />
+                <PartyEditor party={this.state.party} />
+                <EnemyEditor enemies={this.state.enemies} />
             </div>
         );
     }
