1010a87c4546c149d1168b0efe7fd46302622608
Jeremy Wood <farachan@gmail.com>
2017-05-20 12:59:35 -0400

    Added onChange prop to ValueBox.

--- src/components/ValueBox.js
+++ src/components/ValueBox.js
@@ -2,13 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import "../composite.css";
 
-const ValueBox = ({text, value, size, title}) => {
+const ValueBox = ({text, value, size, title, onChange}) => {
     return (
         <div className="combatant__info">
             <span className="combatant__combat-stats-label">{text}</span>
             <input title={title} className="input input--initiative"
                    defaultValue={value} size={size} style={{width: size * 10}}
-                   onClick={(e) => {e.stopPropagation()}} />
+                   onClick={(e) => {e.stopPropagation()}} onChange={() => onChange} />
         </div>
     );
 };
@@ -18,6 +18,7 @@ ValueBox.propTypes = {
     value: PropTypes.number.isRequired,
     size: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
+    onChange: PropTypes.func
 };
 
 export default ValueBox;
