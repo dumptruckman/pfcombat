f9e898c71ea553a40788c0fc3b6d17a05b2d70ab
Jeremy Wood <farachan@gmail.com>
2017-05-21 00:40:58 -0400

    Made ValueBox use value instead of placeholder defaultValue.

--- src/components/ValueBox.js
+++ src/components/ValueBox.js
@@ -7,7 +7,7 @@ const ValueBox = ({text, value, size, title, onChange}) => {
         <div className="combatant__info">
             <span className="combatant__combat-stats-label">{text}</span>
             <input title={title} className="input input--initiative"
-                   defaultValue={value} size={size} style={{width: size * 10}}
+                   value={value} size={size} style={{width: size * 10}}
                    onClick={(e) => {e.stopPropagation()}} onChange={onChange} />
         </div>
     );
