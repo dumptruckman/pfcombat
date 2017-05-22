20be7cbc852862d0dfb0e637aeaac228247f5c57
Jeremy Wood <farachan@gmail.com>
2017-05-21 23:33:54 -0400

    Added onClick prop to ValueButton.

--- src/components/ValueButton.js
+++ src/components/ValueButton.js
@@ -3,18 +3,19 @@ import PropTypes from 'prop-types';
 import "../composite.css";
 import Button from "./Button";
 
-const ValueButton = ({text, value}) => {
+const ValueButton = ({text, value, onClick}) => {
     return (
         <div className="combatant__info">
             <span className="combatant__combat-stats-label">{text}</span>
-            <Button className="button button--hp">{value}</Button>
+            <Button className="button button--hp" onClick={onClick}>{value}</Button>
         </div>
     );
 };
 
 ValueButton.propTypes = {
     text: PropTypes.string.isRequired,
-    value: PropTypes.number.isRequired
+    value: PropTypes.number.isRequired,
+    onClick: PropTypes.func
 };
 
 export default ValueButton;
