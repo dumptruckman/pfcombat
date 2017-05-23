a45bcf7e43b77d80c2bc2ddca73d82cd84801861
Jeremy Wood <farachan@gmail.com>
2017-05-23 14:41:48 -0400

    ValueButton now accept styling for the button.

--- src/components/ValueButton.js
+++ src/components/ValueButton.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import "../composite.css";
 import Button from "./Button";
 
-const ValueButton = ({text, value, onClick}) => {
+const ValueButton = ({text, value, onClick, style}) => {
     return (
         <div className="combatant__info">
             <span className="combatant__combat-stats-label">{text}</span>
-            <Button className="value-button" onClick={onClick}>{value}</Button>
+            <Button className="value-button" style={style ? style : {}} onClick={onClick}>{value}</Button>
         </div>
     );
 };
@@ -15,7 +15,8 @@ const ValueButton = ({text, value, onClick}) => {
 ValueButton.propTypes = {
     text: PropTypes.string.isRequired,
     value: PropTypes.number.isRequired,
-    onClick: PropTypes.func
+    onClick: PropTypes.func,
+    style: PropTypes.object
 };
 
 export default ValueButton;
