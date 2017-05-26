6392090a34579ccb0b094dd5042996c6d403c32a
Jeremy Wood <farachan@gmail.com>
2017-05-26 18:41:14 -0400

    Updates

--- src/CombatantType.js
+++
@@ -1,9 +0,0 @@
-export class CombatantType {
-  constructor(type) {
-    this.type = type;
-  }
-}
-
-export const INITIATIVE = new CombatantType("initiative");
-export const PARTY = new CombatantType("party");
-export const ENEMY = new CombatantType("enemy");
