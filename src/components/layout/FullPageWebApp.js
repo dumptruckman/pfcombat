a8759829d8a6c7d434d5df99f85fb48825a69f9a
Jeremy Wood <farachan@gmail.com>
2017-05-30 16:54:17 -0400

    Switch to styles constant var in FullPageWebApp.

--- src/components/layout/FullPageWebApp.js
+++ src/components/layout/FullPageWebApp.js
@@ -2,23 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 import Radium from "radium";
 
-const FullPageWebApp = (props) => {
-  const styles = {
-    base: {
-      display: "flex",
-      height: "100%",
-      width: "100%",
-      flexDirection: "column",
-      position: "fixed",
-    },
-  };
-
-  return (
-    <div style={styles.base}>
-      {props.children}
-    </div>
-  );
+const styles = {
+  base: {
+    display: "flex",
+    height: "100%",
+    width: "100%",
+    flexDirection: "column",
+    position: "fixed",
+  },
 };
+
+const FullPageWebApp = props => (
+  <div style={styles.base}>
+    {props.children}
+  </div>
+);
 
 FullPageWebApp.propTypes = {
   children: PropTypes.oneOfType([
